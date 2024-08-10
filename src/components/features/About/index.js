e98d9b5b58ce460603c546edbe4f33d0918d7eaa
Abdullahmushaf <shaggyfluppy@gmail.com>
2024-08-10 20:18:40 +0500

    feat: expense tracker app

--- src/components/features/About/index.js
+++ src/components/features/About/index.js
@@ -1,10 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../common/Header";
+import axios from "axios";
 
 const About = () => {
+  const [state, setState] = useState(0);
+  const [input, setInput] = useState("");
+  const [fetchType, setFetchType] = useState("posts");
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    console.log("before return");
+    fetch(`https://jsonplaceholder.typicode.com/${fetchType}`)
+      .then((response) => response.json())
+      .then((d) => setData(d))
+      .then((json) => console.log(json));
+
+    return () => {
+      console.log("return");
+      return setData(null);
+    };
+  }, [fetchType]);
+
+  const incrementHandler = () => {
+    setState((prevState) => prevState + 1);
+    setState((prevState) => prevState + 1);
+    setState((prevState) => prevState + 1);
+  };
+
+  const decrementHandler = () => setState(state - 1);
+
   return (
     <div>
       <Header title="Expense Tracker About" />
+
+      <div>
+        <div>
+          <button type="button" onClick={() => setFetchType("users")}>
+            Users
+          </button>
+          <button type="button" onClick={() => setFetchType("posts")}>
+            Posts
+          </button>
+          <button type="button" onClick={() => setFetchType("todos")}>
+            Todos
+          </button>
+        </div>
+
+        <div>{JSON.stringify(data)}</div>
+      </div>
+
+      <form>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
+
+      <div>
+        <button type="button" onClick={incrementHandler}>
+          +
+        </button>
+        <span>{state}</span>
+        <button type="button" onClick={decrementHandler}>
+          -
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/features/Contact/index.js
+++ src/components/features/Contact/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../common/Header";
 import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
   const navigate = useNavigate();
+  const [contactForm, setContactForm] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    phone: "",
+  });
+
+  const inputChangeHandler = (e) => {
+    console.log(e);
+    setContactForm((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const handler = () => navigate("/");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(contactForm);
+  };
 
   return (
     <div>
@@ -14,22 +33,31 @@ const Contact = () => {
         <section>
           <h1>Contact Details</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam alias
-            saepe quisquam obcaecati porro excepturi illum provident officia
-            harum id eius, officiis necessitatibus maxime cupiditate iste
-            corrupti ipsa. Voluptas delectus minus dolorum similique recusandae
-            itaque fugiat labore magni corrupti culpa illo quidem eius numquam,
-            et excepturi cupiditate, soluta exercitationem tenetur deleniti
-            laboriosam, temporibus earum nobis sit laborum. Quis magni
-            perferendis, dignissimos ipsam doloremque sint dolorem id enim
-            molestias itaque eaque hic? Quisquam unde aperiam velit in quas quae
-            non eos obcaecati, assumenda consequatur dolor nemo error vel
-            veritatis dolore laudantium. Minima obcaecati quam voluptate atque
-            doloremque nemo aut quibusdam dolores.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi animi
+            iusto rerum est impedit ea! Itaque sed repudiandae molestias quo!
           </p>
-          <button type="button" onClick={handler}>
-            Return to Home
-          </button>
+
+          <form onSubmit={submitHandler}>
+            <div>
+              <label htmlFor="fname">First Name</label> <br />
+              <input
+                type="text"
+                name="fname"
+                id="fname"
+                onChange={inputChangeHandler}
+              />
+            </div>
+            <div>
+              <label htmlFor="lname">Last Name</label> <br />
+              <input
+                type="text"
+                name="lname"
+                id="lname"
+                onChange={inputChangeHandler}
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
         </section>
       </main>
     </div>
